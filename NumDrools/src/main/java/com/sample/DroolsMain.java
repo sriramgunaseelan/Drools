369e441f52c42bc8980e6e740070fa121546a945
sriram <sriramcg1509@gmail.com>
2018-07-26 10:20:29 +0530

    A

---
+++ NumDrools/src/main/java/com/sample/DroolsMain.java
@@ -0,0 +1,55 @@
+package com.sample;
+
+import java.math.BigDecimal;
+//import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+public class DroolsMain {
+
+	public static void main(String args[]) {
+
+		String s = JOptionPane.showInputDialog("Enter the number.", null);
+		BigDecimal n = new BigDecimal(s);
+
+		try {
+			// load up the knowledge base
+			KnowledgeBase kbase = readKnowledgeBase();
+			StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+
+			Number n1 = new Number();
+			n1.setNum(n);
+			ksession.insert(n1);
+
+			ksession.fireAllRules();
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	private static KnowledgeBase readKnowledgeBase() throws Exception {
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbuilder.add(ResourceFactory.newClassPathResource("Ruler.drl"), ResourceType.DRL);
+		KnowledgeBuilderErrors errors = kbuilder.getErrors();
+		if (errors.size() > 0) {
+			for (KnowledgeBuilderError error : errors) {
+				System.err.println(error);
+			}
+			throw new IllegalArgumentException("Could not parse knowledge.");
+		}
+		KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+		return kbase;
+	}
+}
---
+++ NumDrools/src/main/java/com/sample/Number.java
@@ -0,0 +1,19 @@
+package com.sample;
+
+import java.math.BigDecimal;
+
+public class Number {
+	
+	BigDecimal num;
+
+	public BigDecimal getNum() {
+		return num;
+	}
+
+	public void setNum(BigDecimal num) {
+		this.num = num;
+	}
+	
+	
+
+}
